b27d5a3b47e6e1fde041b1915d82b12078303e00
Zack Stickles <zstickles@newrelic.com>
2020-12-03 18:32:39 -0800

    chore: first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Agile GitHub
+
+## Local Development
+
+#### Setup
+```sh
+yarn
+```
+
+#### Running
+```sh
+yarn start
+```
+
+## To Do
+* [x] Repository
+* [ ] Query GitHub API
+* [ ] Transform data
+* [ ] Render stuff
---
+++ nerdlets/agile-github-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class AgileGithubNerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, agile-github-nerdlet Nerdlet!</h1>;
+    }
+}
