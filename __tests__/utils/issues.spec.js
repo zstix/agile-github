15788537422ead7c6ec2e172643517ea39dab677
Zack Stickles <zstickles@newrelic.com>
2020-12-09 06:48:14 +0000

    chore: basic return format

--- __tests__/utils/issues.spec.js
+++ __tests__/utils/issues.spec.js
@@ -5,7 +5,7 @@ const mockEvents = (data) =>
   data.map((d) => ({
     previousProjectcolumnName: d.from,
     projectColumnName: d.to,
-    createdAt: `2020-${d.date}T16:49:36Z`,
+    createdAt: `2020-${d.date}T00:00:00Z`,
   }));
 
 const mockIssue = ({ name, points, events }) => ({
@@ -34,35 +34,62 @@ const mockWithResponse = (issues = []) => {
   global.fetch = jest.fn(() => Promise.resolve(resp));
 };
 
+const ISSUES_SIMPLE = [
+  {
+    name: "Test Issue 1",
+    points: 1,
+    events: [
+      { from: "To Do", to: "In Progress", date: "08-24" },
+      { from: "In Progress", to: "For Review", date: "08-25" },
+      { from: "For Review", to: "Done", date: "08-28" },
+    ],
+  },
+  {
+    name: "Test Issue 2",
+    points: 5,
+    events: [
+      { from: "To Do", to: "In Progress", date: "08-25" },
+      { from: "In Progress", to: "For Review", date: "08-27" },
+      { from: "For Review", to: "Done", date: "08-27" },
+    ],
+  },
+];
+
 describe("getPointsForMilestone", () => {
-  it("should return a list of objects", async () => {
-    // expect.assertions(2);
-    expect.assertions(1);
+  describe("Return format", () => {
+    beforeEach(() => {
+      mockWithResponse(ISSUES_SIMPLE);
+    });
 
-    mockWithResponse([
-      {
-        name: "Test Issue 1",
-        points: 1,
-        events: [
-          { from: "To Do", to: "In Progress", date: "08-24" },
-          { from: "In Progress", to: "For Review", date: "08-25" },
-          { from: "For Review", to: "Done", date: "08-28" },
-        ],
-      },
-      {
-        name: "Test Issue 2",
-        points: 5,
-        events: [
-          { from: "To Do", to: "In Progress", date: "08-25" },
-          { from: "In Progress", to: "For Review", date: "08-27" },
-          { from: "For Review", to: "Done", date: "08-27" },
-        ],
-      },
-    ]);
+    it("should return an array", async () => {
+      expect.assertions(1);
 
-    const data = await getPointsForMilestone(OWNER, REPO, MILESTONE, TOKEN);
+      const data = await getPointsForMilestone(OWNER, REPO, MILESTONE, TOKEN);
 
-    expect(data).toBeInstanceOf(Array);
-    // expect(data).toHaveLength(5);
+      expect(data).toBeInstanceOf(Array);
+    });
+
+    it("should return an array of days", async () => {
+      expect.assertions(3);
+
+      const data = await getPointsForMilestone(OWNER, REPO, MILESTONE, TOKEN);
+
+      expect(data).toHaveLength(5);
+      expect(data[0].date).toEqual(new Date("2020-08-24"));
+      expect(data[4].date).toEqual(new Date("2020-08-28"));
+    });
+
+    it("should return an array of columns for each day", async () => {
+      expect.assertions(5);
+
+      const data = await getPointsForMilestone(OWNER, REPO, MILESTONE, TOKEN);
+      const { columns } = data[0];
+
+      expect(columns).toBeInstanceOf(Array);
+      expect(columns[0].label).toEqual("To Do");
+      expect(columns[1].label).toEqual("In Progress");
+      expect(columns[2].label).toEqual("For Review");
+      expect(columns[3].label).toEqual("Done");
+    });
   });
 });
--- src/utils/issues.js
+++ src/utils/issues.js
@@ -2,7 +2,6 @@ import { max, min, differenceInDays, addDays, parseISO } from "date-fns";
 import { prop, get, range } from "./functional";
 import { getIssues } from "./api";
 
-// TODO: FP
 const getDateRange = (arr) => {
   const dates = arr.map(parseISO);
   const start = min(dates);
@@ -11,14 +10,25 @@ const getDateRange = (arr) => {
   return range(numDays).map((n) => addDays(start, n));
 };
 
+const getPointsForDay = (events) => (date) => {
+  return {
+    date,
+    columns: [
+      { label: "To Do" },
+      { label: "In Progress" },
+      { label: "For Review" },
+      { label: "Done" },
+    ],
+  };
+};
+
 export const getPointsForMilestone = async (owner, repo, milestone, token) => {
   try {
     const issues = await getIssues(owner, repo, milestone, token);
-    const dates = issues
-      .flatMap(get("timelineItems.nodes"))
-      .map(prop("createdAt"));
+    const events = issues.flatMap(get("timelineItems.nodes"));
+    const dates = events.map(prop("createdAt"));
 
-    return getDateRange(dates);
+    return getDateRange(dates).map(getPointsForDay(events));
   } catch (e) {
     throw new Error("Unable to calculate points for milestone.");
   }
