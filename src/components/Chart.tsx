1a2aedb0384cab7f2a56d4fa03cbd2e922c08e59
Zack Stickles <zstickles@newrelic.com>
2020-12-24 14:15:18 -0800

    chore: initial chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,86 @@
 import React from 'react';
+import { VictoryStack, VictoryArea } from 'victory';
+import { prop } from '../utils/functional';
 
-const Chart: React.FC = () => (
-  <div>Chart goes here?</div>
-);
+const todo = [
+  { x: 'mon', y: 2 },
+  { x: 'tues', y: 3 },
+  { x: 'wed', y: 5 },
+];
+
+const inProgress = [
+  { x: 'mon', y: 1 },
+  { x: 'tues', y: 4 },
+  { x: 'wed', y: 5 },
+];
+
+const forReview = [
+  { x: 'mon', y: 1 },
+  { x: 'tues', y: 4 },
+  { x: 'wed', y: 5 },
+];
+
+const done = [
+  { x: 'mon', y: 3 },
+  { x: 'tues', y: 2 },
+  { x: 'wed', y: 6 },
+];
+
+interface DataPoint {
+  x: string;
+  y: number;
+}
+
+interface ChartData {
+  todo: DataPoint[];
+  inProgress: DataPoint[];
+  forReview: DataPoint[];
+  done: DataPoint[];
+}
+
+interface ChartState {
+  data: ChartData;
+}
+
+class Chart extends React.Component<null, ChartState> {
+  constructor(props: null) {
+    super(props);
+
+    this.state = { data: null };
+  }
+
+  async componentDidMount(): Promise<void> {
+    try {
+      // TODO: fetch data
+      // TODO: transform
+      // TODO: set state
+      this.setState({ data: { todo, inProgress, forReview, done } });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    const { data } = this.state;
+
+    if (!data) {
+      return null;
+    }
+
+    const { todo, inProgress, forReview, done } = data;
+
+    // TODO: better labels
+    // TODO: better colors
+    // TODO: dynamic columns
+    return (
+      <VictoryStack labels={todo.map(prop('x'))}>
+        <VictoryArea data={todo} />
+        <VictoryArea data={inProgress} />
+        <VictoryArea data={forReview} />
+        <VictoryArea data={done} />
+      </VictoryStack>
+    );
+  }
+}
 
 export default Chart;
